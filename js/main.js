ac6a7216da8cb2f3481fbae86cadba042d5b3ff1
clancysong <clancysong@gmail.com>
2018-02-14 17:39:54 +0800

    eraser finish

--- js/main.js
+++ js/main.js
@@ -8,14 +8,15 @@ const coverPage = () => {
     canvas.height = pageHeight
 }
 
-const drawCircle = (x, y, radius) => {
-    context.fillStyle = 'black'
+const drawCircle = (x, y, width, color) => {
+    context.fillStyle = color
     context.beginPath()
-    context.arc(x, y, radius, 0, Math.PI * 2)
+    context.arc(x, y, width / 2, 0, Math.PI * 2)
     context.fill()
 }
 
-const drawLine = (startPoint, endPoint, lineWidth) => {
+const drawLine = (startPoint, endPoint, lineWidth, color) => {
+    context.strokeStyle = color
     context.beginPath()
     context.moveTo(startPoint.x, startPoint.y)
     context.lineWidth = lineWidth
@@ -28,20 +29,32 @@ const main = () => {
     coverPage()
     window.onresize = coverPage
 
+    const tools = {
+        brush: { id: 1, width: 20, color: '#000000' },
+        eraser: { id: 2, width: 20 }
+    }
+
     let isPressing = false
-    let seleted = 'brush'
+    let selectedId = tools.brush.id
     let lastPoint = {}
+
+    const brush = document.getElementById('brush')
+    const eraser = document.getElementById('eraser')
+
+    brush.onclick = () => { selectedId = tools.brush.id }
+    eraser.onclick = () => { selectedId = tools.eraser.id }
 
     canvas.onmousedown = (event) => {
         const x = event.clientX
         const y = event.clientY
-        switch (seleted) {
-            case 'brush':
-                isPressing = true
+        isPressing = true
+        switch (selectedId) {
+            case tools.brush.id:
                 lastPoint = { x, y }
-                drawCircle(x, y, 10)
+                drawCircle(x, y, tools.brush.width, tools.brush.color)
                 break
-            case 'eraser':
+            case tools.eraser.id:
+                context.clearRect(x, y, tools.eraser.width, tools.eraser.width)
                 break
             default:
                 break
@@ -49,17 +62,17 @@ const main = () => {
     }
 
     canvas.onmousemove = (event) => {
+        if (!isPressing) return
         const x = event.clientX
         const y = event.clientY
-        switch (seleted) {
-            case 'brush':
-                if (isPressing) {
-                    drawCircle(x, y, 10)
-                    drawLine(lastPoint, { x, y }, 20)
-                    lastPoint = { x, y }
-                }
+        switch (selectedId) {
+            case tools.brush.id:
+                drawCircle(x, y, tools.brush.width, tools.brush.color)
+                drawLine(lastPoint, { x, y }, tools.brush.width, tools.brush.color)
+                lastPoint = { x, y }
                 break
-            case 'eraser':
+            case tools.eraser.id:
+                context.clearRect(x, y, tools.eraser.width, tools.eraser.width)
                 break
             default:
                 break
